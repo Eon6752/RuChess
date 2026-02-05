6b14140969319d7bac97dc92f26491cfb283a018
grachevas.27 <yaon777@mail.ru>
2026-02-05 14:11:08 +0300

    test

--- src/DrawingPanel.java
+++ src/DrawingPanel.java
@@ -6,16 +6,15 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;// [2](https://reintech.io/blog/java-2d-graphics-drawing-shapes-text-images)
 //
-//public class Main extends JPanel {
+//public class Game extends JPanel {
 //    private Image image;
-//    public Main() {
+//    public Game() {
 //        try {
 //            image = ImageIO.read(new File("image/black/king.png"));
 //        } catch (IOException e) {
 //            e.printStackTrace();
 //        }
 //    }
-
 
 
 import javax.swing.*;
@@ -26,10 +25,10 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Graphics;
 
-class Main extends JFrame {
-    int count=0;
+ class Game extends JFrame {
+    int count = 0;
 
-    public Main(){
+    public Game() {
 
         super("title");
         setSize(1920, 1080);
@@ -47,17 +46,10 @@ class Main extends JFrame {
 
     }
 
-    public static void main(String[] args) {
-        Main app = new Main();
-        app.setVisible(true);
-        Main m = new Main();
-       m.repaint();
+   
 
-
-
-
-    }
     private Image image;
+
     public void DrawingPanel() {
         try {
             image = ImageIO.read(new File("src/image/black/king.png"));
@@ -65,11 +57,11 @@ class Main extends JFrame {
             e.printStackTrace();
         }
     }
+
     @Override
-    public void paint(Graphics g)
-    {
-        int f=0;
-        Image image1,image2;
+    public void paint(Graphics g) {
+        int f = 0;
+        Image image1, image2,image3,image4,image5,image6;
         try {
             image1 = ImageIO.read(new File("src/image/black/king.png"));
         } catch (IOException e) {
@@ -80,14 +72,64 @@ class Main extends JFrame {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        try {
+            image3 = ImageIO.read(new File("src/image/black/qween.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            image4 = ImageIO.read(new File("src/image/black/officer.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            image5 = ImageIO.read(new File("src/image/black/poni.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            image6 = ImageIO.read(new File("src/image/black/rook.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Image wimage1,wimage2,wimage3,wimage4,wimage5,wimage6;
+        try {
+            wimage1 = ImageIO.read(new File("src/image/white/king.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            wimage2 = ImageIO.read(new File("src/image/white/pon.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            wimage3 = ImageIO.read(new File("src/image/white/qween.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            wimage4 = ImageIO.read(new File("src/image/white/officer.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            wimage5 = ImageIO.read(new File("src/image/white/poni.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            wimage6 = ImageIO.read(new File("src/image/white/rook.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         Color c1 = new Color(255, 0, 0);
-        for(int i=0;i<800;i+=100){
-            for(int j=0;j<800;j+=100){
-                if (f%2==0) {
-                    c1 =new Color(210,105,30);
-                }
-                else{
-                    c1 = new Color(255,228,181);
+        for (int i = 0; i < 800; i += 100) {
+            for (int j = 0; j < 800; j += 100) {
+                if (f % 2 == 0) {
+                    c1 = new Color(210, 105, 30);
+                } else {
+                    c1 = new Color(255, 228, 181);
                 }
                 f++;
                 g.setColor(c1);
@@ -98,8 +140,30 @@ class Main extends JFrame {
         //Image spriteIcon = new Image("src/images/full.png");
         g.drawImage(image, 1000, 500, 500, 500, null);
         g.drawImage(image1, 1033, 105, 34, 90, null);
-        for(int i=600;i<1400;i+=100){
-            g.drawImage(image2, i+38, 230, 23, 40, null);
+        g.drawImage(image3, 933, 105, 34, 90, null);
+        g.drawImage(image4, 833, 105, 34, 90, null);
+        g.drawImage(image4, 1133, 105, 34, 90, null);
+        g.drawImage(image5, 733, 105, 34, 90, null);
+        g.drawImage(image5, 1233, 105, 34, 90, null);
+        g.drawImage(image6, 633, 105, 34, 90, null);
+        g.drawImage(image6, 1333, 105, 34, 90, null);
+
+
+        g.drawImage(image, 1000, 500, 500, 500, null);
+        g.drawImage(wimage1, 1033, 805, 34, 90, null);
+        g.drawImage(wimage3, 933, 805, 34, 90, null);
+        g.drawImage(wimage4, 833, 805, 34, 90, null);
+        g.drawImage(wimage4, 1133, 805, 34, 90, null);
+        g.drawImage(wimage5, 733, 805, 34, 90, null);
+        g.drawImage(wimage5, 1233, 805, 34, 90, null);
+        g.drawImage(wimage6, 633, 805, 34, 90, null);
+        g.drawImage(wimage6, 1333, 805, 34, 90, null);
+        for (int i = 600; i < 1400; i += 100) {
+            g.drawImage(image2, i + 27, 210, 23*2, 40*2, null);
+
+        }
+        for (int i = 600; i < 1400; i += 100) {
+            g.drawImage(wimage2, i + 27, 710, 23*2, 40*2, null);
 
         }
     }
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main extends Game {
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.setVisible(true);
+        Main m = new Main();
+        m.repaint();
+    }
+}
